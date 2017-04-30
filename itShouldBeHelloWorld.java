d69c10b49586a74a1ea224e422f33e1fa210cc5a
Navneet <navneet.sharma.pro@gmail.com>
2017-05-01 00:37:20 +0530

    code #1 Commit #1
    A simple hello world code in java with a pile of comments for beter
    understanding.

---
+++ itShouldBeHelloWorld.java
@@ -0,0 +1,10 @@
+//the very 1st code everyone runs as soon as they jump to a new programming language
+public class itShouldBeHelloWorld
+//public-> if you add me to the class name you have to make sure the code is samved as <classname>.java
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Not Hello World");
+    //println-> I am a method and I print the text.
+  }
+}
