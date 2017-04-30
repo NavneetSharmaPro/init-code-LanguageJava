24d2334859b0791c48ef25f659fb6faa382ab09b
Navneet <navneet.sharma.pro@gmail.com>
2017-05-01 00:50:17 +0530

    printVsPrintln
    How different are out beloved print and println methods?

---
+++ printVsPrintln.java
@@ -0,0 +1,8 @@
+public class printVsPrintln
+{
+  public static void main(String[] args) {
+    System.out.println("better than print as it adds a new line in the end");
+    System.out.print("good for work when you dont need a new line,");
+    System.out.print("like this I came right after the previous string");
+  }
+}
