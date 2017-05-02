d146e12b3d4aa16d6dbbfe5ea6df9be61305709b
Navneet <navneet.sharma.pro@gmail.com>
2017-05-02 14:07:42 +0530

    Works the same
    confirmed.

--- ellipses.java
+++ ellipses.java
@@ -4,5 +4,7 @@ public class ellipses
   public static void main(String... args)
   {
     System.out.println("Thats it, the three dots(...) replacing square brackets[]");
+    int a=Integer.parseInt(args[0]);
+    System.out.println(a);
   }
 }
