2c71a6152f5c9fc9effa3cf2f4914c1b286b9760
Navneet <navneet.sharma.pro@gmail.com>
2017-05-02 14:06:22 +0530

    boringEllipses
    told you, they are boring.

---
+++ ellipses.java
@@ -0,0 +1,8 @@
+//ellipses might be the most stupid thing you ever read
+public class ellipses
+{
+  public static void main(String... args)
+  {
+    System.out.println("Thats it, the three dots(...) replacing square brackets[]");
+  }
+}
