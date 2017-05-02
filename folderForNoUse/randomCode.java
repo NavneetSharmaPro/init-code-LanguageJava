030d53b7a0a1e08e2f35abfd286c36ebd9e0aa4f
Navneet <navneet.sharma.pro@gmail.com>
2017-05-02 14:13:05 +0530

    Testing # to create folders inside my repo
    created a folder on my local machine in git's directry and now syncing
    it, hop it works! cheers~

---
+++ folderForNoUse/randomCode.java
@@ -0,0 +1,6 @@
+public class randomCode
+{
+  public static void main(String[] args) {
+    System.out.println("I don't know why I am writing this but maybe to check weather this process work to create a folder in git repo of mine.");
+  }
+}
