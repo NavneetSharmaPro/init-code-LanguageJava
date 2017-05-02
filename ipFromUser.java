31a545d4403ce28dc8f2f49315bfc419ecc55723
Navneet <navneet.sharma.pro@gmail.com>
2017-05-02 14:03:43 +0530

    howToTakeInputWithDefaultArguments
    Demo to take IP's from user.

---
+++ ipFromUser.java
@@ -0,0 +1,12 @@
+public class ipFromUser
+{
+  public static void main(String[] IcanbeanythingggggBecauseIamjustNameToStringArray)
+  {
+    int a=Integer.parseInt(IcanbeanythingggggBecauseIamjustNameToStringArray[0]);
+    /*
+    taking ip to IcanbeanythingggggBecauseIamjustNameToStringArray
+    converting it into int using Wrapper class Integer
+    copying the value into a integer variable;*/
+    System.out.println(a);
+  }
+}
