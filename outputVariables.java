aa8a7f053b688953f2542df587e649948bb0893e
Navneet <navneet.sharma.pro@gmail.com>
2017-05-02 13:44:23 +0530

    blahWithVariables
    How to print variables and all that stuff.

---
+++ outputVariables.java
@@ -0,0 +1,10 @@
+public class outputVariables
+{
+  public static void main(String[] IcanbeanythingggggBecauseIamjustNameToStringArray)
+  {
+    int a=10;
+    System.out.println(a);//gives 10 as outputVariables
+    a=a+100;
+    System.out.println("With a string you have to do this:"+a);
+  }
+}
