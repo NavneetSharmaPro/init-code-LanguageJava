bd209ab1c02801e6fa49e2a80c4a456572b07788
Navneet <navneet.sharma.pro@gmail.com>
2017-05-04 22:32:42 +0530

    playWithScanner
    Scanner class helps us with the input in java, to take user input so
    that we can further manipulate it to our use.

---
+++ scanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class scanner
+{
+  public static void main(String[] args)
+  {
+    Scanner object_name=new Scanner(System.in);
+    System.out.println(object_name.nextByte());
+    System.out.println(object_name.nextShort());
+    System.out.println(object_name.nextInt());
+    System.out.println(object_name.nextLong());
+    System.out.println(object_name.nextFloat());
+    System.out.println(object_name.nextDouble());
+    System.out.println(object_name.nextBoolean());
+    System.out.println(object_name.nextLine());
+    System.out.println(object_name.next());*/
+  }
+}
